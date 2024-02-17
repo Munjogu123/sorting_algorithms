21183d05e7a36db237fb4304923ce2669d3a8441
Munjogu123 <fredmunjogu@gmail.com>
2024-02-17 16:36:44 +0300

    implements selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * swap - swaps 2 elements
+ * @a: element 1
+ * @b: element 2
+*/
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * selection_sort - implements selection sort
+ * @array: array to be sorted
+ * @size: size of the array
+*/
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		swap(&array[min], &array[i]);
+		print_array(array, size);
+	}
+}
